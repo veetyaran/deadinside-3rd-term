435a53834e20ed04448d965eecb7d40fa1fe9c98
veetyaran <veetyaran@DESKTOP-GJGTOPM.localdomain>
2022-10-26 23:45:48 +0300

    Deleted useless stuff

--- zip/list.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIST_H
-#define LIST_H
-#include "student.h"
-void print_list(int p, list_node* head);
-io_status read_list(const char *name, list_node **head);
-void delete_list(list_node* head);
-#endif
