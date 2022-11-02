0b3cf86fbe9ae9c87055650e34c0df7ca0d2655b
veetyaran <veetyaran@DESKTOP-GJGTOPM.localdomain>
2022-11-02 21:57:40 +0300

    Added Elapsed in main.cpp

--- task6/main.cpp
+++ task6/main.cpp
@@ -1,16 +1,20 @@
 #include "list.h"
-#include <forward_list>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
-    FILE* fp = fopen("test.txt", "r");
+    FILE* fp = fopen("a.txt", "r");
     FILE* fp1 = fopen("test1.txt", "w");
     int max = 10;
+    double time;
     list l;
     l.read(fp);
+    time = clock();
     l.bubbleSort();
+    time = (clock() - time) / CLOCKS_PER_SEC;
     fprintf(fp1, "New List\n");
     l.print(100000000, fp1);
+    printf("Elapsed = %.2f\n", time);
     fclose(fp);
     fclose(fp1);
     
